06fc0b13d95ea4ce5bccbbf079e81b0cd1999a54
lezspb <korplezspb@gmail.com>
2026-04-06 00:11:06 -0400

    v 0.1 05.04.2026 10:55

--- armsa/views.py
+++
@@ -1,6 +0,0 @@
-from django.http.response import HttpResponse
-from django.shortcuts import render
-
-def first_page(request):
-    main_str = "<h1>Hello, World!</h1>"
-    return render(request, '/index.html')
